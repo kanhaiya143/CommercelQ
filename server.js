c0c1b2450e5502fe497c81a561c13191cdf4490e
klmnitt <kanhaiya.nitt@gmail.com>
2020-12-21 22:20:21 +0530

    Added

--- server.js
+++ server.js
@@ -247,3 +247,13 @@ app.delete('/authors/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
